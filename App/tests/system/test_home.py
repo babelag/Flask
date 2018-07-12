3734cf74d688518492b42698471c0d56d33437a1
babelag <hub.lewandowski@o2.pl>
2018-07-12 13:57:05 +0200

    0.1

---
+++ App/tests/system/test_home.py
@@ -0,0 +1,16 @@
+import unittest
+from app import app
+import json
+
+class TestHome(unittest.TestCase):
+    def test_home(self):
+        with app.test_client() as c:
+            response = c.get('/')
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(json.loads
+                             (response.get_data()),
+                             {'message' : 'Hello World !'}
+                             )
+
+            
